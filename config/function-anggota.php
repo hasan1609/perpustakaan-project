78df06980834637a2c5bdfe4e9bfc8a123365941
masterpoku <wafitakputusasa@gmail.com>
2023-02-21 22:22:30 +0700

    backend

--- config/function-anggota.php
+++ config/function-anggota.php
@@ -20,4 +20,29 @@ function anggota()
 
 }
 
+function list_anggota(){
+
+    include 'koneksi.php';
+
+    $sql = "SELECT * FROM `anggota` ORDER BY `anggota`.`id`";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row["id"] . "</td><td> " . $row["nama"] . "</td><td>" . $row["fklts"] . "</td><td>" . $row["jrsn"] . "</td><td> " . $row["level"] . "        <td>
+                      <button type='button' class='btn btn-md btn-social-icon btn-outline-twitter'><i class='ti-pencil'></i></button>
+                      <button type='button' class='btn btn-social-icon btn-outline-youtube'><i class='ti-trash'></i></button>
+                    </td>" ;
+        }
+    } else {
+        echo "0 results";
+    }
+
+echo "</tr>";
+                      
+
+
+}
 ?>
