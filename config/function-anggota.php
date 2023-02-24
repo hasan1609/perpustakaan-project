91d7a9b507460f014964728ec4868c0187a5c9c5
masterpoku <wafitakputusasa@gmail.com>
2023-02-24 19:53:00 +0700

    backend

--- config/function-anggota.php
+++ config/function-anggota.php
@@ -32,12 +32,67 @@ function list_anggota(){
         while ($row = $result->fetch_assoc()) {
             echo "<tr>";
             echo "<td>" . $row["id"] . "</td><td> " . $row["nama"] . "</td><td>" . $row["fklts"] . "</td><td>" . $row["jrsn"] . "</td><td> " . $row["level"] . "        <td>
-         
-            <button name='view' value='". $row["id"]."' class='btn btn-social-icon btn-outline-facebook' href='javascript:void(0)' onClick='viewAnggota()'><i class='ti-printer'></i></button>
-            <button name='print' value='" . $row["id"] . "' class='btn btn-social-icon btn-outline-youtube'><i class='ti-trash'></i></button>
+     
 
 
-                    </td>" ;
+           <button type='button'class='btn btn-social-icon btn-outline-facebook' data-toggle='modal' data-target='#modalAnggota" . $row["id"] . "'><i class='ti-printer'></i></button>
+                   <!-- Button trigger modal -->
+                        <!-- Modal -->
+                        <div class='modal fade' id='modalAnggota".$row['id']."' tabindex='-1' role='dialog' aria-labelledby='modalAnggotaLabel' aria-hidden='true'>
+      <div class='modal-dialog' role='document'>
+        <div class='modal-content'>
+          <div class='modal-header'>
+            <h5 class='modal-title' id='modalAnggotaLabel'>DATA ANGGOTA</h5>
+          </div>
+          <div class='modal-body'>
+            <table>
+              <tr>
+                <td>No. Reg Pendaftaran</td>
+                <td>:</td>
+                <td>sdhgfjd</td>
+              </tr>
+              <tr>
+                <td>Email</td>
+                <td>:</td>
+                <td>sdhgfjd</td>
+              </tr>
+              <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td>" . $row["nama"] . "</td>
+              </tr>
+              <tr>
+                <td>NIDN</td>
+                <td>:</td>
+                <td>sdhgfjd</td>
+              </tr>
+              <tr>
+                <td>Fak/Jur</td>
+                <td>:</td>
+                <td>sdhgfjd</td>
+              </tr>
+              <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                <td>sdhgfjd</td>
+              </tr>
+            </table>
+          </div>
+          <div class='modal-footer'>
+              <button type='button' class='btn btn-secondary' data-dismiss='modal' >Close</button>
+              <button type='button' class='btn btn-primary'>Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+                   </td> 
+
+                    " ;
+
+
+   
+
 
 
         }
@@ -47,8 +102,79 @@ function list_anggota(){
 
 echo "</tr>";
                       
+}
 
+function viewanggota($id){
 
+    
+
+    include 'koneksi.php';
+    $sql = "SELECT * FROM `anggota` WHERE `id` = $id";
+    $result = $conn->query($sql);
+    $rows = $result->fetch_assoc();
+    // echo "TOTAL ANGGOTA BELUM AKTIF: " . $rows["COUNT(*)"] . "  <br><br><br><br>";
+
+    echo "
+    <div class='modal fade' id='modalAnggota' tabindex='-1' role='dialog' aria-labelledby='modalAnggotaLabel' aria-hidden='true'>
+      <div class='modal-dialog' role='document'>
+        <div class='modal-content'>
+          <div class='modal-header'>
+            <h5 class='modal-title' id='modalAnggotaLabel'></h5>
+          </div>
+          <div class='modal-body'>
+            <table>
+              <tr>
+                <td>No. Reg Pendaftaran</td>
+                <td>:</td>
+                <td>sdhgfjd</td>
+              </tr>
+              <tr>
+                <td>Email</td>
+                <td>:</td>
+                <td>sdhgfjd</td>
+              </tr>
+              <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td>" . $rows["nama"] . "</td>
+              </tr>
+              <tr>
+                <td>NIDN</td>
+                <td>:</td>
+                <td>sdhgfjd</td>
+              </tr>
+              <tr>
+                <td>Fak/Jur</td>
+                <td>:</td>
+                <td>sdhgfjd</td>
+              </tr>
+              <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                <td>sdhgfjd</td>
+              </tr>
+            </table>
+          </div>
+          <div class='modal-footer'>
+              <button type='button' class='btn btn-secondary' onclick='closeAnggota()'>Close</button>
+              <button type='button' class='btn btn-primary'>Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<script>
+  function viewAnggota(){
+    // $('#add-group').trigger('reset');
+    $('#modalAnggotaLabel').html('Data Anggota');
+    $('#modalAnggota').modal('show');
+    // $('#id').val('');
+  }
+  
+  function closeAnggota() { 
+    $('#modalAnggota').modal('hide');
+  }
+</script>";
 }
 
 ?>
--- template/header.php
+++ template/header.php
@@ -14,6 +14,9 @@
   <link rel="stylesheet" href="../../vendors/simple-line-icons/css/simple-line-icons.css">
   <link rel="stylesheet" href="../../vendors/css/vendor.bundle.base.css">
   <!-- endinject -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   <!-- Plugin css for this page -->
   <link rel="stylesheet" href="../../vendors/datatables.net-bs4/dataTables.bootstrap4.css">
   <link rel="stylesheet" href="../../js/select.dataTables.min.css">
@@ -25,6 +28,32 @@
   <script src="../../vendors/chart.js/Chart.min.js"></script>
   <!-- select option -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag/dist/css/multi-select-tag.css">
+
+  <!-- plugins:js -->
+  <script src="../../vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <!-- <script src="../../vendors/chart.js/Chart.min.js"></script> -->
+  <script src="../../vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+  <script src="../../vendors/progressbar.js/progressbar.min.js"></script>
+  <!-- <script src="https://code.jquery.com/jquery-3.5.1.js"></script> -->
+  <script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.13.2/js/dataTables.bootstrap4.min.js"></script>
+
+  <!-- select option -->
+  <script src="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag/dist/js/multi-select-tag.js"></script>
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+
+  <script src="../../js/off-canvas.js"></script>
+  <script src="../../js/template.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="../../js/dashboard.js"></script>
+  <script src="../../js/Chart.roundedBarCharts.js"></script>
+  <!-- End custom js for this page-->
+    <!-- Custom js for this page-->
+    <script src="../../js/chart.js"></script>
 </head>
 <body>
     <!-- partial:partials/_navbar.html -->
