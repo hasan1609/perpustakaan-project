ebdcebbe58200eb957d724e6be2de39cc9039159
hasanb1609 <hasanb1609@umsida.ac.id>
2023-02-26 23:45:39 +0700

    absensi

--- pages/absensi/index.php
+++ pages/absensi/index.php
@@ -6,10 +6,10 @@ include '../../template/sidebar.php'
 <div class="main-panel">
   <div class="content-wrapper">
     <div class="row">
-      <div class="col-sm-12 grid-margin stretch-card">
+      <div class="col-sm-5 grid-margin stretch-card">
         <div class="card">
           <div class="card-body">
-            <h4 class="card-title">Data Absensi Pengguna</h4>
+          <h3 class="card-title">Data Absensi Pengguna</h3>
             <br>
             <div class="table-responsive">
               <table class="table table-striped table-bordered" id="example">
@@ -18,12 +18,37 @@ include '../../template/sidebar.php'
                     <th>No</th>
                     <th>Nama</th>
                     <th>Tgl</th>
-                    <th>Opsi</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
                     <td>1</td>
+                    <td>0fhdu</td>
+                    <td>bukuku</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-7 grid-margin stretch-card">
+        <div class="card">
+          <div class="card-body">
+          <h3 class="card-title">Permintaan Absensi</h3>
+            <br>
+            <div class="table-responsive">
+              <table class="table table-striped table-bordered" id="absensi2">
+                <thead>
+                  <tr>
+                  <tr>
+                    <th>Nama</th>
+                    <th>Tgl</th>
+                    <th>Opsi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
                     <td>0fhdu</td>
                     <td>bukuku</td>
                     <td>
