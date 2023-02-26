658ebe2a46c2e446b13de1f3b346dcc2b7acfed2
masterpoku <wafitakputusasa@gmail.com>
2023-02-26 18:34:39 +0700

    Update function-anggota.php

--- config/function-anggota.php
+++ config/function-anggota.php
@@ -105,7 +105,16 @@ function anggota()
     return $rows["COUNT(*)"];
 
 }
+function anggota_baru()
+{
+    include 'koneksi.php';
+    $sql = "SELECT COUNT(*) FROM `anggota` WHERE `status` = 0 ORDER BY `id` DESC;";
+    $result = $conn->query($sql);
+    $rows = $result->fetch_assoc();
+    // echo "TOTAL ANGGOTA BELUM AKTIF: " . $rows["COUNT(*)"] . "  <br><br><br><br>";
+    return $rows["COUNT(*)"];
 
+}
 function list_anggota(){
 
     include 'koneksi.php';
