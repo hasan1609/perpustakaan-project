7fd463cc64a9bf62ad602227f208de72c1fe5459
masterpoku <wafitakputusasa@gmail.com>
2023-03-02 18:42:40 +0700

    Update function-denda.php

--- config/function-denda.php
+++ config/function-denda.php
@@ -1,28 +1,36 @@
 <?php
 
 
-function Denda(){
+function Denda()
+{
     include 'koneksi.php';
 
-    $sql = "SELECT * FROM `p_buku`";
+    $sql = "SELECT * FROM `denda`";
     $result = $conn->query($sql);
 
 
     $i = 0;
     while ($row = $result->fetch_assoc()) {
-        $awal = date_create($row['tgl_kmbl']);
+        $awal = date_create($row['created']);
         $akhir = date_create(); // waktu sekarang
         $diff = date_diff($awal, $akhir);
-        $denda  = number_format($diff->days * 500, 2, ',', '.');
+        $denda = number_format($diff->days * 500, 2, ',', '.');
 
         echo "  <tr>
                         <td>1</td>
-                        <td>".$row['nama']."</td>
-                        <td>".$row['jdl_buku']."</td>
-                        <td>" . $row['tgl_kmbl'] . "</td>
-                        <td>". $denda."</td>
+                        <td>" . $row['nama'] . "</td>
+                        <td>" . $row['jdl_buku'] . "</td>
+                        <td>" . $row['status'] . "</td>
+                        <td>" . $denda . "</td>
                         <td>
-                            <label for='' class='badge badge-danger'>Lunas</label>
+                        ";
+                        if ($row['status'] == '1') {
+                           echo "<label for='' class='badge badge-success'>Lunas</label>";
+                        }else {
+                           echo "<label for='' class='badge badge-danger'>Belum Lunas</label>";
+                        }
+                        echo "
+                            
                         </td>
                         <td>
                         <button type='button' class='btn btn-primary btn-icon-text'>
